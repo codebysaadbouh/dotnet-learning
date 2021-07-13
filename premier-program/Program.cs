8274e8ff89b2fe7d3ea1a97943ededeb482c260e
codebysaadbouh <74369527+codebysaadbouh@users.noreply.github.com>
2021-07-13 23:52:23 +0200

    Update Code - Boucle remis sur l'ensemble

--- premier-program/Program.cs
+++ premier-program/Program.cs
@@ -1,52 +1,42 @@
-﻿using System; 
+﻿using System;
 
 namespace premier_program
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            
+
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            /*
+
             // Déclaration de variable
-            int age_int =0;
+            int age_int = 0;
             string age_str;
             string nom;
-
             // Demande le nom de la personne
             Console.WriteLine("Quel est votre nom ?");
             nom = Console.ReadLine();
 
-            // Demande l'age de la personne
-            Console.WriteLine("Vous avez quel age ?");
-            age_str = Console.ReadLine();
-            
-            // On va essayer de ...
-            try
+            while (age_int == 0)
             {
-                age_int = int.Parse(age_str);
-                age_int += 1;
+                // Demande l'age de la personne
+                Console.WriteLine("Vous avez quel age ?");
+                age_str = Console.ReadLine();
 
-                Console.WriteLine("Vous vous appellez " + nom + " et vous avez " + age_str + " an(s)");
-                Console.WriteLine("Bientôt vous aurez " + age_int + " an(s)");
-            }
-            catch
-            {
-                Console.WriteLine("Vous n'avez pas saisi votre age au bon format");
+                try
+                {
+                    age_int = int.Parse(age_str);
+                }
+                catch
+                {
+                    Console.WriteLine("Vous n'avez pas saisi votre age au bon format");
+                }
             }
 
-            */
-
-            int i = 0;
-            // Boucle While
-            while (i <= 10)
-            {
-                Console.WriteLine("2 x " + i + " = " + 2 * i);
-                i++;  // Incrémentation de la valeur i; 
-            }
-            Console.WriteLine("Fin de boucle ! "); 
+            Console.WriteLine("Vous vous appellez " + nom + " et vous avez " + age_int + " an(s)");
+            age_int += 1;
+            Console.WriteLine("Bientôt vous aurez " + age_int + " an(s)");
 
         }
     }
